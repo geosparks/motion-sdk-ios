ce8cf13bab3050dfb2091b10afed5f34bd8d92a4
Ravikanth <ravikanth@geospark.co>
2021-10-15 18:24:26 +0530

    1.0.26

---
+++ MotionSDK/MotionSDK.xcframework/ios-arm64_x86_64-simulator/MotionSDK.framework/Headers/MotionSDK.h
@@ -0,0 +1,19 @@
+//
+//  MotionSDK.h
+//  MotionSDK
+//
+//  Created by GeoSpark Mac 15 on 24/08/20.
+//  Copyright © 2020 GeoSpark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MotionSDK.
+FOUNDATION_EXPORT double MotionSDKVersionNumber;
+
+//! Project version string for MotionSDK.
+FOUNDATION_EXPORT const unsigned char MotionSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MotionSDK/PublicHeader.h>
+
+
